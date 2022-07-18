fe68bfaafda26146eebae4bfe76d688da6706037
vivian <vivian.v18723@gmail.com>
2022-07-18 16:05:50 +0100

    edit variables

--- client/assets/scripts/index.js
+++ client/assets/scripts/index.js
@@ -3,19 +3,19 @@ function displayRestaurantDetails() {
   heading.textContent = data["name"];
 
   const year = document.getElementById("establishedYear");
-  heading.textContent = data["name"];
+  year.textContent = data["name"];
 
   const status = document.getElementById("restaurantStatus");
-  heading.textContent = data["name"];
+  status.textContent = data["name"];
 }
 
 function loadRestaurantDetails() {
   const url = "http://localhost:3000";
 
   fetch(url)
-    .then(res => res.json())
-    .then(data => displayRestaurantDetails(data)) // need this 2nd .then() as the 1st one is returning a promise
-    .catch(err => {
+    .then((res) => res.json())
+    .then((data) => displayRestaurantDetails(data)) // need this 2nd .then() as the 1st one is returning a promise
+    .catch((err) => {
       console.log(err);
     });
 }
